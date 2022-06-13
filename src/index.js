ce4c3256d7747d1cd58ddf72ee9988b7fc774037
Matheus Silva <matheussilva.fga@gmail.com>
2022-06-13 13:40:06 -0300

    Creating server with express

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const { response } = require("express");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("Hello World!");
+});
+
+app.listen(3001, () => console.log("Server started at http://localhost:3001"));
