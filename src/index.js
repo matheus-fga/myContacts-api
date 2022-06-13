7364161e4a7b601c84b79f6669cfdefceb4b66f7
Matheus Silva <matheussilva.fga@gmail.com>
2022-06-13 14:17:20 -0300

    Configuring code formatter ESLint and editorconfig

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-const { response } = require("express");
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send("Hello World!");
+app.get('/', (request, response) => {
+  response.send('Hello World!');
 });
 
-app.listen(3001, () => console.log("Server started at http://localhost:3001"));
+app.listen(3001, () => console.log('Server started at http://localhost:3001'));
