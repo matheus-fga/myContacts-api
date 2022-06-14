d701ab8229429871c498023bf11f04224f3f00b4
Matheus Silva <matheussilva.fga@gmail.com>
2022-06-14 16:04:51 -0300

    Refactoring repository delete method to remove contact from db

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,22 +1,4 @@
-const { v4 } = require('uuid');
 const db = require('../../database');
-
-let contacts = [
-  {
-    id: v4(),
-    name: 'Matheus',
-    email: 'matheus@mail.com',
-    phone: '1212121212',
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'João',
-    email: 'joao@mail.com',
-    phone: '1212121212',
-    category_id: v4(),
-  },
-];
 
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
@@ -62,11 +44,9 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
+  async delete(id) {
+    const deleteOp = await db.query('DELETE FROM contacts WHERE id = $1', [id]);
+    return deleteOp;
   }
 }
 
