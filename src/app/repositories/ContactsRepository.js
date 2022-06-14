c48babe9bae54758dd6c9b6ce37b183fd1336a73
Matheus Silva <matheussilva.fga@gmail.com>
2022-06-14 15:13:29 -0300

    Refactoring repository find methods to use db

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -19,16 +19,19 @@ let contacts = [
 ];
 
 class ContactsRepository {
-  findAll() {
-    return new Promise((resolve) => resolve(contacts));
+  async findAll() {
+    const rows = await db.query('SELECT * FROM contacts');
+    return rows;
   }
 
-  findByID(id) {
-    return new Promise((resolve) => resolve(contacts.find((contact) => contact.id === id)));
+  async findByID(id) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE id=$1', [id]);
+    return row;
   }
 
-  findByEmail(email) {
-    return new Promise((resolve) => resolve(contacts.find((contact) => contact.email === email)));
+  async findByEmail(email) {
+    const [row] = await db.query('SELECT * FROM contacts WHERE email=$1', [email]);
+    return row;
   }
 
   delete(id) {
