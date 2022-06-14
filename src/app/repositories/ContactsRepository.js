bea545d16ce71ebc4729008284117701a35e042d
Matheus Silva <matheussilva.fga@gmail.com>
2022-06-14 15:52:14 -0300

    Refactoring repository update method to use db

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -26,20 +26,13 @@ class ContactsRepository {
   }
 
   async findByID(id) {
-    const [row] = await db.query('SELECT * FROM contacts WHERE id=$1', [id]);
+    const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [id]);
     return row;
   }
 
   async findByEmail(email) {
-    const [row] = await db.query('SELECT * FROM contacts WHERE email=$1', [email]);
+    const [row] = await db.query('SELECT * FROM contacts WHERE email = $1', [email]);
     return row;
-  }
-
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
   }
 
   async create(contact) {
@@ -48,30 +41,31 @@ class ContactsRepository {
     } = contact;
 
     const [row] = await db.query(`INSERT INTO contacts(name, email, phone, category_id)
-       VALUES($1, $2, $3, $4)
-       RETURNING *
+    VALUES($1, $2, $3, $4)
+    RETURNING *
     `, [name, email, phone, category_id]);
 
     return row;
   }
 
-  update(id, contactInfo) {
+  async update(id, contact) {
     const {
       name, email, phone, category_id,
-    } = contactInfo;
+    } = contact;
 
+    const [row] = await db.query(`
+      UPDATE contacts
+      SET name = $1, email = $2, phone = $3, category_id = $4
+      WHERE id = $5
+      RETURNING *
+    `, [name, email, phone, category_id, id]);
+    return row;
+  }
+
+  delete(id) {
     return new Promise((resolve) => {
-      const updatedContact = {
-        id,
-        name,
-        email,
-        phone,
-        category_id,
-      };
-      contacts = contacts.map((contact) => (
-        contact.id === id ? updatedContact : contact
-      ));
-      resolve(updatedContact);
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
     });
   }
 }
