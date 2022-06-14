67262970671a0f6453c680e8037b9b260295c0c1
Matheus Silva <matheussilva.fga@gmail.com>
2022-06-14 14:56:21 -0300

    Refactoring repository create method to use db

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,4 +1,5 @@
 const { v4 } = require('uuid');
+const db = require('../../database');
 
 let contacts = [
   {
@@ -37,22 +38,17 @@ class ContactsRepository {
     });
   }
 
-  create(contact) {
+  async create(contact) {
     const {
       name, email, phone, category_id,
     } = contact;
 
-    return new Promise((resolve) => {
-      const newContact = {
-        id: v4(),
-        name,
-        email,
-        phone,
-        category_id,
-      };
-      contacts.push(newContact);
-      resolve(newContact);
-    });
+    const [row] = await db.query(`INSERT INTO contacts(name, email, phone, category_id)
+       VALUES($1, $2, $3, $4)
+       RETURNING *
+    `, [name, email, phone, category_id]);
+
+    return row;
   }
 
   update(id, contactInfo) {
